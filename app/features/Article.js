49a435dcf598165ad138731458cb5e0728dfee81
Jawad Bangash <jawadbanga@gmail.com>
2024-02-22 01:38:34 -0800

    news articles added

--- app/features/Article.js
+++ app/features/Article.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Image from 'next/image';
-import styles from '../../styles/Article.module.css';
+// import Image from 'next/image';
+import styles from '../styles/Article.module.css';
 
 const Article = ({ title, excerpt, imageUrl }) => (
   <article className={styles.article}>
